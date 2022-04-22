95adf2631bd1a50c0b0668d42234c918bd0d686f
Tschokkinen <tschokkinen@gmail.com>
2022-04-22 21:47:27 +0300

    Started working on activity_report_pain.

--- app/src/main/java/com/ericaskari/healthapplication/modules/painhistory/NewPainLogActivity.java
+++ app/src/main/java/com/ericaskari/healthapplication/modules/painhistory/NewPainLogActivity.java
@@ -1,18 +1,32 @@
 package com.ericaskari.healthapplication.modules.painhistory;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ericaskari.healthapplication.R;
 import com.ericaskari.healthapplication.databinding.NewPainLogBinding;
+
+/**
+ * @author Gavril Tschokkinen
+ */
 
 public class NewPainLogActivity extends AppCompatActivity {
     private NewPainLogBinding NewPainLogBinding;
+    private Spinner selectPainSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         NewPainLogBinding = NewPainLogBinding.inflate(getLayoutInflater());
         setContentView(NewPainLogBinding.getRoot());
+
+        selectPainSpinner = findViewById(R.id.selectPainSpinner);
+        //https://developer.android.com/guide/topics/ui/controls/spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list_of_pains, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectPainSpinner.setAdapter(adapter);
     }
 }

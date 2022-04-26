8d6b6b98b3ecf846c398d7f3d5c1a9bc115d9d3d
Eric Askari <eric.askari@weare.fi>
2022-04-26 15:20:17 +0300

    Pain history list added.

--- app/src/main/java/com/ericaskari/healthapplication/modules/painhistory/PainHistoryActivity.java
+++ app/src/main/java/com/ericaskari/healthapplication/modules/painhistory/PainHistoryActivity.java
@@ -3,16 +3,34 @@ package com.ericaskari.healthapplication.modules.painhistory;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
+import com.ericaskari.healthapplication.adapters.PainLogListRecyclerViewAdapter;
 import com.ericaskari.healthapplication.databinding.PainHistoryBinding;
+import com.ericaskari.healthapplication.models.PainLog;
+import com.ericaskari.healthapplication.services.AppDatabase;
+
+import java.util.List;
 
 public class PainHistoryActivity extends AppCompatActivity {
     private PainHistoryBinding painHistoryBinding;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        painHistoryBinding = PainHistoryBinding.inflate(getLayoutInflater());
+        this.painHistoryBinding = PainHistoryBinding.inflate(getLayoutInflater());
+        this.db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "app-db").allowMainThreadQueries().build();
+
         setContentView(painHistoryBinding.getRoot());
+
+        List<PainLog> data = this.db.painLogDao().getAll();
+
+        PainLogListRecyclerViewAdapter adapter = new PainLogListRecyclerViewAdapter(data, getApplication());
+
+        this.painHistoryBinding.painHistoryRecyclerView.setAdapter(adapter);
+
+        this.painHistoryBinding.painHistoryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
